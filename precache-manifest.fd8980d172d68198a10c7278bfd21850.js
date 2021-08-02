b7c10164d716f4c8a20328bb675a10fbfba26749
Andrii Balitskyi <10balian10@gmail.com>
2021-08-02 18:08:18 +0300

    Updates

---
+++ precache-manifest.fd8980d172d68198a10c7278bfd21850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a08375384052084bfa91e471a4ec37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f0f314d46702eba70595",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "939e6f9d8bfe7c63afb6",
+    "url": "./static/js/2.5b7a4cf6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5b7a4cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f314d46702eba70595",
+    "url": "./static/js/main.d7685b5d.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
